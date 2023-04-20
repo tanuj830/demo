fc3001ba37790073100a07d3549e85f271ca5ac2
tanuj <[artravellers98@gmail.com]>
2023-04-21 02:44:39 +0530

    first commit

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -33,72 +33,12 @@ const Navbar = () => {
 
   return (
     <>
-      {/* for mobile */}
-      <div className=" inline md:hidden ">
-        <div className=" container flex justify-between items-center gap-4 md:gap-44 shadow-sm py-1 ">
-        <div className="flex items-center  py-4">
-            <Link className="text-md md:text-2xl text-themecol flex items-center justify-center" href="/home">
-              <BsAirplaneEngines /><span className="ml-2">ARTravells</span>
-            </Link>
-          </div>
-          <div className="w-full relative">
-                <form action="GET" className="flex  items-center">
-                <input className="w-full py-1 md:py-2 px-2 md:px-5 border-none rounded-full bg-slate-200" type="text" placeholder="Search" name="search" onChange={e=>setQuery(e.target.value)} />  
-            <div className="absolute right-2">
-                <button type="submit"  onClick={handleSubmit}  className="p-1 text-slate-500"><BsSearch /></button> 
-            </div>
-                </form>    
-          </div>
-          <div className="ml-2">
-            {!showMenu ? (
-              <button
-                className="text-2xl mr-3 "
-                onClick={handleClick}
-              >
-                <RxHamburgerMenu />
-              </button>
-            ) : (
-              <button
-                className="text-2xl mr-3 "
-                onClick={handleClick}
-              >
-                <RxCross1 />
-              </button>
-            )}
-          </div>
-        </div>
-        <div className="mb-2 md:container flex items-center justify-between">
-
-          {/* <div> */}
-            {showMenu == true ?  (
-              // <div className="flex mt-6 flex-col w-full ">
-              // <div className="flex  items-center justify-start w-full">
-              //     <div className="flex flex-col w-full scroll-0">
-              //     <Link className="flex items-center gap-1" href='/'> <h3 className="ml-8"><TbBrandBlogger/></h3><h3>Blogs</h3></Link>                     
-              //               <Link className="flex items-center gap-1" href='/contact'> <h3 className="ml-8 text-xl"><AiOutlineContacts/></h3><h3>Contact Us</h3></Link>  
-              //     <hr />
-              //     </div>
-              // </div>
-
-              // </div>
-              <div className="relative w-full h-full py-10">
-                  <div className="absolute bg-white w-full">
-                  <Link className="flex items-center gap-1 my-3" href='/'> <h3 className="ml-8"><TbBrandBlogger/></h3><h3>Blogs</h3></Link>                     
-             <Link className="flex items-center gap-1 my-3" href='/contact'> <h3 className="ml-8 text-xl"><AiOutlineContacts/></h3><h3>Contact Us</h3></Link>  
-                  </div>
-              </div>
-            ): null}
-          {/* </div> */}
-        </div>
-        {/* <hr /> */}
-      </div>
-
       {/* forpc */}
       <div className=" bg-white  hidden md:inline tracking-widest">
         <div className=" py-3 shadow-md container flex items-center justify-between w-100 ">
           <div className="flex items-center ">
             <Link className="text-3xl text-themecol flex items-center justify-center" href="/">
-              <BsAirplaneEngines/><span className="ml-2">ARTravells</span>
+              <span className="ml-2">Dose of Bhabhar</span>
             </Link>
           </div>
           <div className="">
